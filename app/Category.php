a7640e070bbcd7f8753793e7db48eba2bb816d59
(Felipe <felipe.esbu@gmail.com>
2021-01-06 23:25:17 -0300

    cambios a modelo Category

--- app/Category.php
+++ app/Category.php
@@ -3,8 +3,36 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Services\Exist;
 
 class Category extends Model
 {
+    protected $table = 'category';
+    protected $primaryKey = 'ID';
+
+    public function items(){
+        return $this->hasMany(\App\Item::class);
+    }
+
+    // Ver si la categoría existe en la BD
+    static public function exist($category){
+        $count = Category::where('ID', $category)->count();
+        return Exist::exist($count);
+    }
     
+    //Crear una nueva categoría
+    static public function createCategory($id, $name){
+        $category = new Category();
+
+        $category->id = $id;
+        $category->name   = $name;
+
+        $category->save();
+    }
+
+    // Obtener todas las categorías
+    static public function getCategories(){
+        return Category::all();
+    }
+
 }
